6a4d3b4029bdb88d2e0014454e1e0f4fe4ddc763
canknbr <cankanbur1@gmail.com>
2023-04-07 12:17:52 +0300

    added sample

--- eggTimer/ViewController.swift
+++ eggTimer/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer : AVAudioPlayer?
+    var eggTimer = ["Soft":3,"Medium":5,"Hard":7]
+    var totalTime = 0
+    var currentTime = 0
+    var timer = Timer()
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        progressBar.progress = 0.0
     }
 
-
+    @IBAction func eggPressed(_ sender: UIButton) {
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        totalTime = eggTimer[hardness]!
+        progressBar.progress = 0.0
+        currentTime = 0
+        labelText.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(eggCalc), userInfo: nil, repeats: true)
+        
+        
+        
+    }
+    @objc func eggCalc(){
+    
+        if currentTime < totalTime {
+            currentTime += 1
+            progressBar.progress = Float(currentTime) / Float(totalTime)
+            labelText.text = String(totalTime - currentTime)
+        }else{
+            timer.invalidate()
+            labelText.text = "Done"
+            playSound()
+        }
+        
+    }
+    
+    
+    
+    
+    func playSound(){
+        guard let path = Bundle.main.path(forResource: "alarm_sound", ofType: "mp3") else {
+            return
+        }
+        
+        let url = URL(filePath: path)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        }catch let error{
+            print(error.localizedDescription)
+        }
+        
+    }
+    
 }
 
