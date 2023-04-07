835f060c217d999c837ac7d175b47fdf763e50c7
canknbr <cankanbur1@gmail.com>
2023-04-07 11:59:33 +0300

    Initial Commit

---
+++ eggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eggTimer
+//
+//  Created by Can Kanbur on 7.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
